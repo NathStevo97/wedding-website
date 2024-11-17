8a92a1249a4a3f5871abd09d4950c53ce3c4b52d
NathStevo97 <natstephenson15@gmail.com>
2024-11-17 19:11:43 +0000

    chore: remove pocs and add favicons

--- infra/modules/cloudfront/main.tf
+++ infra/modules/cloudfront/main.tf
@@ -52,53 +52,12 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     target_origin_id = var.domain_name
     cache_policy_id  = var.caching_policy_id
 
-    /* forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    } */
-
-    /* lambda_function_association {
-      event_type   = "viewer-request"
-      include_body = false
-      lambda_arn   = "arn:aws:lambda:us-east-1:862772511843:function:test-python-site-auth:1"
-    } */
-
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 360
     max_ttl                = 86400
     compress               = true
   }
-
-  # Dynamic block for static asset cache behaviors
-  dynamic "ordered_cache_behavior" {
-    for_each = ["css", "js", "jpg", "jpeg", "png"]
-    content {
-      path_pattern     = "*/*.${ordered_cache_behavior.value}"
-      allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-      cached_methods   = ["GET", "HEAD"]
-      target_origin_id = var.domain_name
-      cache_policy_id  = var.caching_policy_id
-
-      /* forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    } */
-
-      viewer_protocol_policy = "redirect-to-https"
-      min_ttl                = 0
-      default_ttl            = 86400    # 24 hours
-      max_ttl                = 31536000 # 1 year
-      compress               = true
-    }
-  }
-
 
   viewer_certificate {
     cloudfront_default_certificate = false
--- pocs/README.md
+++
@@ -1,2 +0,0 @@
-# wedding-website
-Repository to host source code for the Wedding Website for the wedding of myself and my lovely fiancee.
