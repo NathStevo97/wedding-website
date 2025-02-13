d1ad3ec80eedeb300a114ff511b0d5e0f920cb08
NathStevo97 <natstephenson15@gmail.com>
2025-02-13 10:17:00 +0000

    chore: update responsiveness for menu containers and remove logging

--- infra/modules/S3/logging.tf
+++ infra/modules/S3/logging.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "logging_bucket" {
+/* resource "aws_s3_bucket" "logging_bucket" {
   bucket        = "${local.www_domain}-logs"
   force_destroy = true
   tags = var.tags
@@ -16,4 +16,4 @@ resource "aws_s3_bucket_logging" "redirect_logging" {
 
   target_bucket = aws_s3_bucket.logging_bucket.id
   target_prefix = "logs/${var.domain_name}/"
-}
+} */
