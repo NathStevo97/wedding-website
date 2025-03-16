8c97f9b757377fb54d4481d0ecf39a1ca3305b6d
NathStevo97 <natstephenson15@gmail.com>
2025-03-16 09:38:34 +0000

    chore: aws logging public access block

--- infra/modules/S3/main.tf
+++ infra/modules/S3/main.tf
@@ -85,8 +85,18 @@ resource "aws_s3_bucket" "logging_bucket" {
   tags   = var.tags
 }
 
+resource "aws_s3_bucket_public_access_block" "logging_public_access" {
+  bucket = aws_s3_bucket.logging_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_acl" "log_bucket_acl" {
   bucket = aws_s3_bucket.logging_bucket.id
+  depends_on = [aws_s3_bucket_ownership_controls.static_website_ownership]
   acl    = "log-delivery-write"
 }
 
@@ -95,4 +105,11 @@ resource "aws_s3_bucket_logging" "logging_config" {
 
   target_bucket = aws_s3_bucket.logging_bucket.id
   target_prefix = "log/"
+}
+
+resource "aws_s3_bucket_versioning" "logging_versioning" {
+  bucket = aws_s3_bucket.logging_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
