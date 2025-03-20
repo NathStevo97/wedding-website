61dccfa457d32aa01bd204d3d5fc5c16778663ef
NathStevo97 <natstephenson15@gmail.com>
2025-03-20 21:04:54 +0000

    chore: aws provider update

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.9.8"
+  required_version = ">= 1.11.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.89.0"
+      version = ">= 5.92.0"
     }
     null = {
       source  = "hashicorp/null"
